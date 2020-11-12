15dc2714613345ebda9402a8eec908c8c85cd29c
04alexklink <alex_klink123@hotmail.co.uk>
2020-11-12 11:05:39 +0000

    Notelist object can be instantiated

---
+++ spec/NotelistSpec.js
@@ -0,0 +1,8 @@
+describe('Notelist', function() {
+  var notelist;
+
+  it('can be instantiated', function() {
+    notelist = new Notelist();
+    expect(notelist).toBeInstanceOf(Notelist);
+  })
+});
