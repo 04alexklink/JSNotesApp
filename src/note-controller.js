0a205e6e005110852f663106906a9589f29e285a
04alexklink <alex_klink123@hotmail.co.uk>
2020-11-17 21:12:57 +0000

    note-controller.js includes code that creates a new note, note list and note list view, and sets the html to be equal to the note content, utilising all 3 models

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,7 +1,9 @@
 var app = document.getElementById('app');
-var note = new Note("Take out bins");
+var note1 = new Note("Take out bins");
+var note2 = new Note("Take out the bins again")
 var notelist = new Notelist();
-notelist.addNote(note);
+notelist.addNote(note1);
+notelist.addNote(note2);
 var notelistview = new Notelistview(notelist);
-// console.log(notelistview);
-// app.innerHTML=`${notelistview.viewNotes()}`;
+console.log(notelistview);
+app.innerHTML=`${notelistview.viewNotes()}`;
