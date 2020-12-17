d5e05202997337f1ab0a11d5927f8ab3e463528a
04alexklink <alex_klink123@hotmail.co.uk>
2020-12-17 11:13:44 +0000

    Injected dependency in the constructor of note-controller. And moved JS code interacting with the DOM into it's own file interface.js

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,6 +1,8 @@
 class NoteController {
-  constructor(notelist) {
+  constructor(notelist = new Notelist(), notelistview = new Notelistview(), singlenoteview = new SingleNoteView()) {
     this._notelist = notelist;
+    this._notelistview = notelistview;
+    this._singlenoteview = singlenoteview;
   }
 
   addNote(notetext) {
@@ -10,35 +12,19 @@ class NoteController {
   displayNotes() {
     if(this._notelist.returnNotes().length >0) {
     var app = document.getElementById('app');
-    var notelistview = new Notelistview(this._notelist);
-    app.innerHTML= notelistview.viewNotesFirst20Chars();
+    app.innerHTML= this._notelistview.viewNotesFirst20Chars(this._notelist);
     }
   }
 
   displayIndividualNote(idofnote) {
     this._notelist.returnNotes().forEach((note) => {
       if(note.viewID() == idofnote) {
-        var singlenoteview = new SingleNoteView()
         var app = document.getElementById('app')
-        app.innerHTML= `${singlenoteview.showNote(note)}`
+        app.innerHTML= `${this._singlenoteview.showNote(note)}`
       }
     })
   }
 }
 
-window.addEventListener("hashchange", function() {
-  var noteid = window.location.hash.split('#notes/')[1]
-  notecontroller.displayIndividualNote(noteid)
-})
 
-document.getElementById('createnote').addEventListener('submit', function(event) {
-  event.preventDefault();
-  newnotetext = document.getElementById('notetocreate').value;
-  notecontroller.addNote(newnotetext);
-  notecontroller.displayNotes();
-})
-
-var notelist = new Notelist;
-notecontroller = new NoteController(notelist)
-notecontroller.displayNotes()
 
